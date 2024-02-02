094a11930d2f1fe14cf14299a34f3b8e80cbe638
gichohi <fgkaruga@gmail.com>
2024-02-02 19:46:16 +0300

    Update Solution.java
    Updated maxVowels

--- java/src/main/java/org/leetcode/maxvowels/Solution.java
+++ java/src/main/java/org/leetcode/maxvowels/Solution.java
@@ -4,41 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
+
     public static int maxVowels(String s, int k) {
-        int max_vowels = 0;
-        int count = 0;
-        List<String> charList = Arrays.asList(s.split(""));
-        String[] vowels = new String[]{"a", "e", "i", "o", "u"};
-        List<String> list = Arrays.asList(vowels);
-
-        for (int i = 0; i < k; i++) {
-            if (list.contains(charList.get(i))) {
-                count++;
-            }
-        }
-        max_vowels = Math.max(max_vowels, count);
-
-        int left = 1;
-        int right = left + k - 1;
-
-        while (right < charList.size()) {
-            String word = charList.get(right);
-            String prev = charList.get(left - 1);
-            if (list.contains(word)) {
-                count++;
-            }
-            if (list.contains(prev)) {
-                count--;
-            }
-            max_vowels = Math.max(max_vowels, count);
-            left++;
-            right++;
-        }
-
-        return max_vowels;
-    }
-
-    public static int maxi(String s, int k) {
         String vowels = "aeiou";
         int maxVowels = 0;
         int currentVowels = 0;
