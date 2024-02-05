62babd1d3eb9cfc0db7ca09678891c899b166568
Gichohi Karuga <fgkaruga@gmail.com>
2024-02-05 22:50:39 +0300

    added number of islands test

--- java/src/test/java/numberOfIslands/SolutionTests.java
+++ java/src/test/java/numberOfIslands/SolutionTests.java
@@ -1,4 +1,38 @@
 package numberOfIslands;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.leetcode.numberOfIslands.Solution;
+
 public class SolutionTests {
+
+    @Test
+    void testNumIslands() {
+        Solution solutionTest = new Solution();
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        int expected = 3;
+        int actual = solutionTest.numIslands(grid);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void testNumIslandsTwo() {
+        Solution solutionTest = new Solution();
+        char[][] grid = {
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'1', '1', '0', '1', '1'}
+        };
+
+        int expected = 5;
+        int actual = solutionTest.numIslands(grid);
+        Assertions.assertEquals(actual, expected);
+    }
 }
