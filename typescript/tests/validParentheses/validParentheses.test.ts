ead872486dce0994d49ddb2315be441e281bb962
Gichohi Karuga <fgkaruga@gmail.com>
2024-02-09 22:04:32 +0300

    fixed error

--- typescript/tests/validParentheses/validParentheses.test.ts
+++ typescript/tests/validParentheses/validParentheses.test.ts
@@ -3,7 +3,7 @@ import { isValid } from "../../src/validParentheses/validParentheses";
 describe('testing parentheses validity', () => {
     it('should return true for valid parentheses and else false', () => {
         const valid = "{{()}}";
-        const invalid = "[}";
+        const invalid = "[()}[]]";
         expect(isValid(valid)).toEqual(true);
         expect(isValid(invalid)).toEqual(false);
     });
