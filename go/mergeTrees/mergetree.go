44fa145413842958161a1785112403125739ed37
Gichohi <gichohi@PC109-C02YT33ELVDL.local>
2024-02-16 18:35:00 +0300

    added same tree

---
+++ go/mergeTrees/mergetree.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	res := &TreeNode{}
+	if root1 != nil && root2 != nil {
+		res.Val = root1.Val + root2.Val
+	} else if root1 == nil {
+		res.Val = root2.Val
+	} else if root2 == nil {
+		res.Val = root1.Val
+	}
+	res.Left = mergeTrees(root1.Left, root2.Left)
+	res.Right = mergeTrees(root1.Left, root2.Left)
+	return res
+}
+
+func main() {
+	root1 := &TreeNode{Val: 1}
+	root1.Left = &TreeNode{Val: 3}
+	root1.Right = &TreeNode{Val: 2}
+
+	root2 := &TreeNode{Val: 2}
+	root2.Left = &TreeNode{Val: 1}
+	root2.Right = &TreeNode{Val: 3}
+
+	fmt.Println(mergeTrees(root1, root2))
+}
